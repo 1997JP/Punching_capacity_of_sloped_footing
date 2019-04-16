7f170bd5cf73e2d07c30d4e7cd333aeedb256bf3
1997JP <47134014+1997JP@users.noreply.github.com>
2019-04-16 11:00:16 +0530

    Update main (1).m

--- main (1).m
+++ main (1).m
@@ -5,22 +5,22 @@ load given_data.mat
 
 #to find effective depth 
 disp("To find effective depth") 
-disp("D_effective = D- clear_cover-dia/2")
-D_effective = D- clear_cover-dia/2;
+disp("D_effective = Depth_at_face- clear_cover-dia/2")
+D_effective = Depth_at_face- clear_cover-dia/2;
 printf("D_effective = % d mm \n",D_effective)
 printf("\n")
 
 #by using similar traingle property
 disp("by using similar traingle property")
-disp("x=((D_effective/2)*(D-d))/((B-b)/2)")
-x=((D_effective/2)*(D-d))/((B-b)/2);
+disp("x=((D_effective/2)*(Depth_at_face-Depth_at_edge))/((length-Side_of_column)/2)")
+x=((D_effective/2)*(Depth_at_face-Depth_at_edge))/((length-Side_of_column)/2);
 printf("x = % d mm \n",x)
 printf("\n")
 
 #to find depth where crack occurs
 disp("To find depth where crack occurs")
-Depth=D-x;
-disp("Depth=D-x")
+Depth=Depth_at_face-x;
+disp("Depth=Depth_at_face-x")
 printf("Depth = % d mm \n",Depth)
 Depth_effective=Depth-clear_cover-dia/2;
 disp("Depth_effective=Depth-clear_cover-dia/2")
@@ -29,7 +29,7 @@ printf("\n")
 
 #To find punching capacity
 disp("To find punching capacity")
-Punching_capacity = (.25*sqrt(fck)*Depth_effective*4*(b+(Depth_effective/2)))/(1000*1.5);
-disp("Punching_capacity = (.25*sqrt(fck)*Depth_effective*4*(b+(Depth_effective/2)))/(1000*1.5)")
+Punching_capacity = (.25*sqrt(fck)*Depth_effective*4*(Side_of_column+(Depth_effective/2)))/(1000*1.5);
+disp("Punching_capacity = (.25*sqrt(fck)*Depth_effective*4*(Side_of_column+(Depth_effective/2)))/(1000*1.5)")
 printf("Punching_capacity = % d kN \n",Punching_capacity)
 
